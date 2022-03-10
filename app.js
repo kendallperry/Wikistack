c490a0d0ec8a3424b4f9f47167577c2b93cdf851
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-03-10 15:26:52 -0500

    connect to database and set up Page and User models

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const { db } = require("./models");
+const path = require("path");
 const app = express();
-const PORT = 5432;
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
+  console.log(".get working");
   res.send("<h1>Hello world!</h1>");
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server listening on Port: ${PORT}`));
