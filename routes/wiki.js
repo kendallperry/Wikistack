994de35f242f59f3ab24b2d773b60a6b83f0de99
Kendall Perry <perry.kendall@gmail.com>
2022-03-10 18:01:24 -0500

    getting to html

--- routes/wiki.js
+++ routes/wiki.js
@@ -11,8 +11,20 @@ router.get("/add", function (req, res) {
   res.send(addPage());
 });
 
-router.post("/", function (req, res) {
-  res.send("got to POST /wiki/");
+router.post('/', async (req, res, next) => {
+
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+
+  try {
+    const page = await Page.create({
+      title: res.body.title,
+      content: res.body.content,
+    });
+
+    // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
+    res.redirect('/');
+  } catch (error) { next(error) }
 });
 
 module.exports = router;
